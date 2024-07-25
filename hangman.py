15bd20fcaf38a62c4a8953be4efa6a90be03552e
FelicityBlue <zinfelicity@gmail.com>
2024-07-25 09:37:40 -0400

    added a function to compare player input to the answer

--- hangman.py
+++ hangman.py
@@ -59,3 +59,12 @@ words = ('ant baboon badger bat bear beaver camel cat clam cobra cougar '
          'rhino salmon seal shark sheep skunk sloth snake spider '
          'stork swan tiger toad trout turkey turtle weasel whale wolf '
          'wombat zebra ').split()
+
+def check_answer(player_guess, ans, guess ):
+  
+  for i in range(0, len(str(player_guess))):
+    if i < len(ans):
+      if player_guess[i] == ans[i]:
+        guess[i] = ans[i]
+    else:
+      break
